b4ac826a36d5d49a6374b2423d711d3a9b0e192b
Patrick Attema <p.attema@digitalden.nl>
2019-04-06 23:41:23 +0200

    Add Webpack

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+import * as tf from '@tensorflow/tfjs-node-gpu';
+
+import processData from './js/helpers';
+import plotData from './js/plot';
+
 /*
     Build Convolutional Neural Network using Tensorflow.js
 */
@@ -81,7 +86,7 @@ const cnn = function (model, data, epochs) {
                 /*for (let i = result.epoch.length-1; i < result.epoch.length; ++i) {
                     print("Loss after Epoch " + i + " : " + result.history.loss[i]);
                 }*/
-                print("Loss after last Epoch (" + result.epoch.length + ") is: " + result.history.loss[result.epoch.length-1]);
+                print("Loss after last Epoch (" + result.epoch.length + ") is: " + result.history.loss[result.epoch.length - 1]);
                 resolve(model);
             })
         }
@@ -92,7 +97,7 @@ const cnn = function (model, data, epochs) {
 }
 
 let url = 'https://api.iextrading.com/1.0/stock/%company%/chart/1y'
-let epochs = 100;
+let epochs = 1200;
 let timePortion = 7;
 
 $(document).ready(function () {
@@ -104,23 +109,23 @@ $(document).ready(function () {
         clearPrint();
         print("Beginning Stock Prediction ...");
         let company = $('#company').val().trim();
-        
+
         $.getJSON(url.replace('%company%', company).toLowerCase()).then(function (data) {
-            
+
             // Get the datetime labels use in graph
             let labels = data.map(function (val) { return val['date']; });
-            
+
             // Process the data and create the train sets
             processData(data, timePortion).then(function (result) {
-                
+
                 // Crate the set for stock price prediction for the next day
                 let nextDayPrediction = generateNextDayPrediction(result.originalData, result.timePortion);
                 // Get the last date from the data set
-                let predictDate = (new Date(labels[labels.length-1] + 'T00:00:00.000')).addDays(1);
+                let predictDate = (new Date(labels[labels.length - 1] + 'T00:00:00.000')).addDays(1);
 
                 // Build the Convolutional Tensorflow model
                 buildCnn(result).then(function (built) {
-                    
+
                     // Transform the data to tensor data
                     // Reshape the data in neural network input format [number_of_samples, timePortion, 1];
                     let tensorData = {
@@ -130,23 +135,23 @@ $(document).ready(function () {
                     // Rember the min and max in order to revert (min-max scaler) the scaled data later 
                     let max = built.data.max;
                     let min = built.data.min;
-                    
+
                     // Train the model using the tensor data
                     // Repeat multiple epochs so the error rate is smaller (better fit for the data)
                     cnn(built.model, tensorData, epochs).then(function (model) {
-                        
+
                         // Predict for the same train data
                         // We gonna show the both (original, predicted) sets on the graph 
                         // so we can see how well our model fits the data
                         var predictedX = model.predict(tensorData.tensorTrainX);
-                        
+
                         // Scale the next day features
                         let nextDayPredictionScaled = minMaxScaler(nextDayPrediction, min, max);
                         // Transform to tensor data
                         let tensorNextDayPrediction = tf.tensor1d(nextDayPredictionScaled.data).reshape([1, built.data.timePortion, 1]);
                         // Predict the next day stock price
                         let predictedValue = model.predict(tensorNextDayPrediction);
-                        
+
                         // Get the predicted data for the train set
                         predictedValue.data().then(function (predValue) {
                             // Revert the scaled features, so we get the real values
@@ -161,7 +166,7 @@ $(document).ready(function () {
                                 predictedXInverse.data = Array.prototype.slice.call(predictedXInverse.data);
                                 // Add the next day predicted stock price so it's showed on the graph
                                 predictedXInverse.data[predictedXInverse.data.length] = inversePredictedValue.data[0];
-    
+
                                 // Revert the scaled labels from the trainY (original), 
                                 // so we can compare them with the predicted one
                                 var trainYInverse = minMaxInverseScaler(built.data.trainY, min, max);
@@ -173,16 +178,15 @@ $(document).ready(function () {
                             // Print the predicted stock price value for the next day
                             print("Predicted Stock Price of " + company + " for date " + moment(predictDate).format("DD-MM-YYYY") + " is: " + inversePredictedValue.data[0].toFixed(3) + "$");
                         });
-                        
+
                     });
-                    
+
                 });
-                
+
             });
-            
+
         });
-        
-        
+
+
     });
 });
-    
